3943523de3be9e4337e7e827d70af2a505624d1f
Marco Seguri <seguri@users.noreply.github.com>
2020-10-06 16:40:35 +0200

    Add bean validation

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -3,12 +3,15 @@ package tacos.web;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.Ingredient;
 import tacos.Taco;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -41,7 +44,11 @@ public class DesignTacoController {
     }
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid @ModelAttribute("design") Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
+
         // Save the taco design...
         // We'll do this in chapter 3
         log.info("Processing design: " + design);
