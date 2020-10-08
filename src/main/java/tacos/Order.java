a1172219348ccddb8ecec9c667a94b68ff85685d
Marco Seguri <seguri@users.noreply.github.com>
2020-10-08 09:32:38 +0200

    Update models

--- src/main/java/tacos/Order.java
+++ src/main/java/tacos/Order.java
@@ -6,13 +6,15 @@ import org.hibernate.validator.constraints.CreditCardNumber;
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @Data
 public class Order {
 
     private Long id;
-    private Date createdAt;
+    private Date placedAt;
 
     @NotBlank(message = "Name is required")
     private String name;
@@ -32,9 +34,15 @@ public class Order {
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
 
-    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([1-9][0-9])$", message = "Must be formatted MM/YY")
+    @Pattern(regexp = "^(0[1-9]|1[0-2])([/])([1-9][0-9])$", message = "Must be formatted MM/YY")
     private String ccExpiration;
 
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
+
+    private List<Taco> tacos = new ArrayList<>();
+
+    public void addDesign(Taco design) {
+        tacos.add(design);
+    }
 }
--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,7 +2,6 @@ package tacos;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -18,6 +17,6 @@ public class Taco {
     @Size(min = 5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotEmpty(message = "You must choose at least 1 ingredient")
-    private List<String> ingredients;
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients;
 }
