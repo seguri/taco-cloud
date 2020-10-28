ac8b78a69feb0ae7b33fc2baafc573ec1b781c28
Marco Seguri <seguri@users.noreply.github.com>
2020-10-29 00:48:45 +0100

    Split into multiple modules

---
+++ taco-cloud-web/src/test/java/tacos/TacoCloudWebApplication.java
@@ -0,0 +1,8 @@
+package tacos;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudWebApplication {
+    // Allows @WebMvcTest annotation to be autoconfigured
+}
